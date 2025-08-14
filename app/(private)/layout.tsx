ffff246103783a37283b3ec97f20217666e6d25d
vinaycreative <vnaycreative@gmail.com>
2025-08-14 16:07:23 +0530

    feat: minor changes in layout

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header"
 
 export default function PrivateLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-dvh grid grid-rows-[1fr_70px] bg-gray-50">
+    <div className="h-dvh grid grid-rows-[1fr_70px] bg-white min-w-[320px] max-w-[400px] mx-auto">
       {/* <Header title="Money Manager" /> */}
       <main className="overflow-y-auto">{children}</main>
       <BottomNavigation />
