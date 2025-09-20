25846e7cd3a12290ee1e877f96e48721f342ba13
Vinay <vnaycreative@gmail.com>
2025-09-21 01:09:08 +0530

    fix: supabse db type issue resolved

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,13 +1,12 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr"
 import { cookies } from "next/headers"
-import { type Database } from "@/types/supabase"
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const createClient = async () => {
   const cookieStore = await cookies()
-  return createServerClient<Database>(supabaseUrl!, supabaseKey!, {
+  return createServerClient(supabaseUrl!, supabaseKey!, {
     cookies: {
       getAll() {
         return cookieStore.getAll()
