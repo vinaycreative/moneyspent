1bd49e422745b4f1055fac2002245f976ba6648f
Vinay <vnaycreative@gmail.com>
2025-09-21 02:10:10 +0530

    Update package dependencies and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import TanstackProvider from "@/context/TanstackProvider"
 import SessionMonitor from "@/components/SessionMonitor"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -55,6 +57,7 @@ export default function RootLayout({
         <TanstackProvider>
           <SessionMonitor />
           {children}
+          <SpeedInsights />
         </TanstackProvider>
       </body>
     </html>
